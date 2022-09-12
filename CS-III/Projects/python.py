ed958154da1ad25a926b899033a477f8c504aa5f
Jacob Jershin <jane@williams.org>
2022-09-12 12:22:53 -0500

    commit all

--- CS-III/Projects/python.py
+++ CS-III/Projects/python.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3.8
-print("test")
+import js2py
+
+#Array = [input()] #last step is to make the array the inp
+
+Array = ["case","farse","abba","aabb","a","fartstat"]
+
+js = '''
+function sort(Array) {
+for (let i = 0; i < Array.length; i++) {
+for (let compare = 0; compare < Array.length; compare++) {
+if (Array[i] < Array[compare]) {
+Temp = Array[i]
+Array[i] = Array[compare]
+Array[compare] = Temp
+}
+}
+}
+return (Array)
+}
+'''
+res = js2py.eval_js(js)
+
+print(res(Array))
