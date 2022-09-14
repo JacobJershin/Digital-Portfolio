3147178d9e760aa5c1d11cbffcab6132d1123f5a
Jacob Jershin <jane@williams.org>
2022-09-14 11:59:37 -0500

    commit all

--- CS-III/Projects/python.py
+++ CS-III/Projects/python.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3.8
 import js2py
+import sys
 
-#Array = [input()] #last step is to make the array the inp
+Array = []
+out = ""
 
-Array = ["case","farse","abba","aabb","a","fartstat"]
+while True: #while theres still standard input going in
+    try: # it will try to add that input to the array
+        Array.append(input('')) #adds thge input 
+    except EOFError: #go until the input is done (crlt D)
+        break #ends 
+
+
+#Array = ["case","farse","abba","aabb","a","fartstat"]
+
+def sort(Array):
+    for i in range(1, len(Array)):
+        j = i-1
+        while (Array[j] > Array[i]) and (j >= 0):
+            Array[j+1] = Array[j]
+            j=j-1
+        
+            
 
 js = '''
-function sort(Array) {
+function sort(Array) { 
 for (let i = 0; i < Array.length; i++) {
 for (let compare = 0; compare < Array.length; compare++) {
 if (Array[i] < Array[compare]) {
