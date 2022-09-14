c0f272ad7d20673ada53489a9105ae12a0274d47
Jacob Jershin <jane@williams.org>
2022-09-14 12:51:21 -0500

    fasdfasdf

--- CS-III/Projects/python.py
+++ CS-III/Projects/python.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3.8
-import js2py
-import sys
-
 Array = []
 
 while True: #while theres still standard input going in
@@ -10,40 +7,11 @@ while True: #while theres still standard input going in
     except EOFError: #go until the input is done (crlt D)
         break #ends 
 
+Temp = []
+while Array:
+    smallest = min(Array)
+    Temp.append(smallest)
+    Array.pop(Array.index(smallest))
 
-#Array = ["case","farse","abba","aabb","a","fartstat"]
-
-#def sort(Array):
-#    for i in range(1, len(Array)):
-#        j = i-1
-#        Place = Array[i]
-#        while (Array[j] > Array[i]) and (j >= 0):
-#            Array[j+1] = Array[j]
-#            j=j-1
-#        Array[j+1] = Array[j]
-
-def sort(Array):
-    return (quicksort([x for x in Array[1:] if x <  Array[0]])
-            + [Array[0]] +
-            quicksort([x for x in Array[1:] if x >= Array[0]]))
-
-print(sort(Array))        
-
-
-#js = '''
-#function sort(Array) { 
-#for (let i = 0; i < Array.length; i++) {
-#for (let compare = 0; compare < Array.length; compare++) {
-#if (Array[i] < Array[compare]) {
-#Temp = Array[i]
-#Array[i] = Array[compare]
-#Array[compare] = Temp
-#}
-#}
-#}
-#return (Array)
-#}
-#'''
-#res = js2py.eval_js(js)
-
-#print(res(Array))
+print(Temp)
+    
