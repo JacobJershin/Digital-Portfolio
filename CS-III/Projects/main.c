e69e57ff6a07dcb667bdf0aedd158e8e2e0b5855
Jacob Jershin <jane@williams.org>
2022-09-16 11:53:22 -0500

    add all

---
+++ CS-III/Projects/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int c;
+  c = getchar();
+
+  char 
+  int i,j;
+
+  for (i = 0; i < 10; i++) {
+    Array[i] = i + 100;
+  }
+
+  for (j = 0; j < 10; i++) {
+    printf("element[%d] = %d\n", j, Array[j] );
+  }
+	   
+  
+  return 0;
+}
