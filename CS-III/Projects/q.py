a74e27f79e53105c2f56d3499d0424b5f25804cd
Jacob Jershin <jane@williams.org>
2022-09-23 12:26:27 -0500

    snannnek

---
+++ CS-III/Projects/q.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.9
+A = []
+while True: #while theres still standard input going in
+    try: # it will try to add that input to the array
+        A.append(input('')) #adds thge input
+    except EOFError: #go until the input is done (c
+        break
+
+def q(a):
+    if len(a) <= 1:
+        return a
+    else:
+        return q([i for i in a[1:] if i < a[0]]) + [a[0]] + q([i for i in a[1:] if i >= a[0]])
+print(q(A))
